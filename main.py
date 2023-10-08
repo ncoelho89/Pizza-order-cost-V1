cf5861d54cf688f9ae1c197c188cc746c130e5e3
ncoelho89 <144150634+ncoelho89@users.noreply.github.com>
2023-10-08 15:07:57 -0400

    Update main.py
    Version 1.1 
    streamlined code

--- main.py
+++ main.py
@@ -6,25 +6,23 @@ extra_cheese = input("Do you want extra cheese for an additional cost? Y or N\n"
 bill = 0
 
 if size == "S":
-  bill = 15
-  if add_pepperoni == "Y":
-    bill += 2
-  if extra_cheese == "Y":
-      bill += 1
+  bill += 15
 elif size == "M":
-  bill = 20
-  if add_pepperoni == "Y":
-    bill += 3
-  if extra_cheese == "Y":
-      bill += 1 
-elif size == "L":
-  bill = 25
-  if add_pepperoni == "Y":
-    bill += 3
-  if extra_cheese == "Y":
-      bill += 1
-  
-print(bill)
+  bill += 20
+else:
+  bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S"
+    bill += 2
+  else:
+      bill += 3
+    
+if extra_cheese == "Y":
+  bill += 1
+
+print(f"Your final bill is ${bill})
+
 
 
 
